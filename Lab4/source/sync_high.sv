dca6c9c6efca0b2798d31420091dafea308272c2
Jiahao Xu <xu1392@purdue.edu>
2020-09-16 14:21:59 -0400

    update

---
+++ Lab4/source/sync_high.sv
@@ -0,0 +1,21 @@
+// $Id: $
+// File name:   sync_high.sv
+// Created:     9/16/2020
+// Author:      Jiahao Xu
+// Lab Section: 337-002
+// Version:     1.0  Initial Design Entry
+// Description: .
+
+module sync_high (
+    clk, 
+    n_rst,
+    async_in,
+    sync_out,
+);
+    input logic clk, n_rst, async_in;
+    output logic sync_out;
+    always_ff @ (posedge clk, negedge n_rst) begin
+        if (n_rst == 1'b0) sync_out <= 1'b1;
+        else sync_out <= async_in;
+    end
+endmodule
