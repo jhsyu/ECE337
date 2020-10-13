69462c46bade0fc5475609cfd8a658274386bab8
Jiahao Xu <xu1392@purdue.edu>
2020-10-13 10:35:30 -0400

    add new source file.

---
+++ Lab7/source/controller.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   controller.sv
+// Created:     10/13/2020
+// Author:      Jiahao Xu
+// Lab Section: 337-002
+// Version:     1.0  Initial Design Entry
+// Description: .
