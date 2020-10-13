7f2eb16371f89ec6896c0b3c90e5c887efeb344c
xu1392 <xu1392@purdue.edu>
2020-10-13 11:04:26 -0400

    update counter.sv

--- Lab7/source/counter.sv
+++ Lab7/source/counter.sv
@@ -5,3 +5,15 @@
 // Lab Section: 337-002
 // Version:     1.0  Initial Design Entry
 // Description: .
+module counter(clk, n_rst, cnt_up, clear, one_k_samples);
+    input wire clk, n_rst, cnt_up, clear, one_k_samples;
+    localparam NUM_BITS = 10;
+    localparam VAL = 1000;
+    flex_counter #(NUM_BITS)(.clk(clk), 
+                             .n_rst(n_rst), 
+                             .clear(clear), 
+                             .count_enable(cnt_up), 
+                             .rollover_val(VAL), 
+                             .rollover_flag(one_k_samples)
+                             );
+endmodule
